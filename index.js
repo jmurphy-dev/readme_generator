5bdee7538c781fccff8639ad2f0fed34734ef779
Jack Murphy <signal.space@protonmail.com>
2020-10-13 19:38:36 -0500

    added questions

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
 // array of questions for user
-const iq = require("inquirer");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const gM = require("./generateMarkdown.js")
 
-const questions = [
-
-];
-
-// function to write README file
-const writeToFile = (fileName, data)  => {
-
-}
-
-// iife init
-( () => {
-    console.log("Init!");
-})();
-
+const questions = inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Description?",
+      name: "description",
+    },
+    {
+      // TODO this should allow multiple intputs
+      type: "input",
+      message: "Table of Content?",
+      name: "table_of_contents",
+    },
+    {
+      type: "input",
+      message: "Installation?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Usage?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "License?",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Contributing?",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Tests?",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Questions?",
+      name: "questions",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+    const filename = README_DEMO.md;
+    fs.appendFile(filename, gM(response), (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(response);
+      }
+    });
+  });
 
 
