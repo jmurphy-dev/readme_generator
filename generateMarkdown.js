ce7f8f20b410c161ddbec137ee5d43ad40364084
Jack Murphy <signal.space@protonmail.com>
2020-10-15 20:30:28 -0500

    more badge styling

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,7 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return ` #${data.title}
-  <a href=""><img src="https://img.shields.io/npm/l/${data.license}"></a>
+  return `# ${data.title}
+  <a href=""><img src="https://img.shields.io/badge/<License>-<${data.license}>"></a>
   ---
 
 ${data.description}
